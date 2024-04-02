b03c675e7ac08ccf636e56e04a5c7f0f970891e5
unknown <nitya142001@gmail.com>
2024-04-02 17:40:34 +0530

    navbar and form

---
+++ NavBar/navbar.js
@@ -0,0 +1,4 @@
+// document.getElementById("toggleSidebar").addEventListener("click", function() {
+//     var sidebar = document.getElementById("mySidebar");
+//     sidebar.style.display = sidebar.style.display === "none" ? "block" : "none";
+// });
