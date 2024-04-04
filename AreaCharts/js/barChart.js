4451d0a2d1a8bc0b0b9550e16992c8772b205896
unknown <nitya142001@gmail.com>
2024-04-04 17:59:54 +0530

    chart

---
+++ AreaCharts/js/barChart.js
@@ -0,0 +1,26 @@
+const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['C', 'C++', 'Pyhton', 'Java', 'JavaScript', 'C#'],
+      datasets: [{
+        label: '# languages',
+        data: [20, 30, 50, 20, 60, 10],
+       
+      },
+      {
+        data:[30,40,10,20,40,10]
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+    options:{
+      responsive:false
+    }
+  });
